741aa4a91635d44c0efc1b0434138458f0db13aa
maryam <maryamosman0099@gmail.com>
2026-03-12 12:02:48 +0200

    Fix script path in index.html, update JavaScript notes, and enhance script.js with additional examples

--- Sessions/session_5/js/script.js
+++ Sessions/session_5/js/script.js
@@ -22,7 +22,7 @@ console.log(fruit.length) //prints 3, it counts the number of sub-arrays in the 
 console.log(fruit[1].length)
  //prints 3, it counts the number of elements in the sub-array at index 1
  **/
-let fruit =["apple","banana","grapes","orange","mango","kiwi","berry"];
+/**let fruit =["apple","banana","grapes","orange","mango","kiwi","berry"];
 console.log(fruit.slice(2,3,"apple")) //prints ["grapes"], it returns a new array containing the elements from index 2 to index 3 (excluding index 3)
 console.log(fruit.slice(2,0,"apple")) //prints [], it returns an empty array because the start index is greater than the end 
 console.log(fruit.slice(2));
@@ -34,4 +34,43 @@ fruit.includes("grapes") //prints true, it returns true because "grapes" is foun
 console.log(fruit.sort()) //prints ["apple","banana","berry","grapes","kiwi","mango","orange"], it sorts the array in alphabetical order
 console.log(fruit.reverse()) //prints ["orange","mango","kiwi","grapes","berry","banana","apple"], it reverses the order of the elements in the array
 
+fruit1=fruit.concat(["watermelon","papaya"]) //creates a new array by concatenating the original array with the new array
 
+for (let i=0;i<fruit1.length;i++){ //creates a new array by concatenating the original array with the new array
+console.log(fruit1) //prints first then check the condition
+    if(fruit1[i]== "banana"){
+        break;
+    }
+//console.log(fruit1[i]) //prints the elements of the array until it finds "banana"
+}
+
+for(let i=1;i<12;i++){
+   
+        console.log(`2 * ${i} = ${2*i}`) //prints the multiplication table of 2 from 1 to 11
+    
+}
+for(let i=1;i<12;i++){
+    for(let j=1;j<12;j++){ // finishes first loop then starts the second loop
+        console.log(`${i} * ${j} = ${i*j}`) //prints the multiplication table from 1 to 11
+    }
+}
+
+let i =1;
+while(i<=12){
+    console.log(i)
+    i++;
+}
+let num =prompt("Enter a number");
+while(num<10 || num>50){ // the propmt stops when entering a correct number 
+    console.log("Number is less than 10");
+    num = prompt("Enter a number");
+}
+console.log(`the number is ${num}`)
+**/
+let str = prompt("Enter a string");
+
+while(str!="admin"){
+console.log("Incorrect password");
+str = prompt("Enter a string");
+}
+console.log("Correct password entered");
