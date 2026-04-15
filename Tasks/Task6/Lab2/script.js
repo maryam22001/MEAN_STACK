bb7035e4197a4d3db8e1fb4250073260d5c4e8ee
maryam <maryamosman0099@gmail.com>
2026-04-15 12:37:47 +0200

    fix

--- Tasks/Task6/Lab2/script.js
+++ Tasks/Task6/Lab2/script.js
@@ -1,31 +1,47 @@
 let clock = document.getElementById("clock");
-let timer = null;  
-let start =document.getElementById("start");
-let end =document.getElementById("stop");
+let startBtn = document.getElementById("start");
+let stopBtn = document.getElementById("stop");
+
+let timer = null;
+function format(num) {
+    return num.toString().padStart(2, '0');
+}
+
 
 function runClock() {
-   
-    clock.textContent = new Date().toLocaleTimeString();            
+    const now = new Date();
+
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
+
+    clock.textContent =
+        format(hours) + ":" +
+        format(minutes) + ":" +
+        format(seconds);
 }
+
 function startClock() {
     if (timer === null) {
-        runClock();
-        timer = setInterval(runClock);
+        runClock(); // update immediately
+        timer = setInterval(runClock, 1000);
     }
 }
 
 function stopClock() {
     clearInterval(timer);
-    timer = null; 
+    timer = null;
 }
-startClock();
 
+startBtn.addEventListener("click", startClock);
+stopBtn.addEventListener("click", stopClock);
 
-
-window.addEventListener('keydown', function(e) {
-    if (e.key.toLowerCase() === 's') {
+window.addEventListener("keydown", function (e) {
+    if (e.key.toLowerCase() === "s") {
         startClock();
-    } else if (e.key.toLowerCase() === 'x') {
+    } else if (e.key.toLowerCase() === "x") {
         stopClock();
     }
 });
+
+startClock();
